702dd72289398a199177d958eb7c8e41421526c9
unknown <ramalhoitalo26@gmail.com>
2026-05-16 15:41:05 -0300

    first commit

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 // Global Application Environment Variables
 export const CONFIG = {
   // Replace with your production server URL later (e.g., https://yourdomain.com)
-  API_URL: "https://wallet-app-pe7z.onrender.com"
+  API_URL: "https://wallet-app-pe7z.onrender.com/api"
 };
